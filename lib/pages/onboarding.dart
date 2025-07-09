ae4465cef7bb78de7634748488410d492793d45a
Tinh Dang <dangductinh1103@gmail.com>
2025-07-09 23:24:12 +0700

    update navigate onboarding

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/pages/home.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -11,78 +12,80 @@ class OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Image.asset(
-              "images/onboard.jpg",
-              height: MediaQuery.of(context).size.height / 1.7,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Image.asset(
+            "images/onboard.jpg",
+            height: MediaQuery.of(context).size.height / 1.7,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height / 1.9,
             ),
-            Container(
-              margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height / 1.9,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
               ),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular((30)),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 20.0),
+                Text(
+                  "Get The Latest And Updated\nNews Easily With Us",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-
-              child: Column(
-                children: [
-                  SizedBox(height: 20.0),
-                  Text(
-                    "Get The Latest And Updated\nNews Easily With Us",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Image.asset(
-                    "images/newspaper.png",
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 30),
-                  Container(
-                    margin: (EdgeInsets.only(left: 40 , right: 40)),
-                    child: Material(
-                      elevation: 3.0,
+                SizedBox(height: 20),
+                Image.asset(
+                  "images/newspaper.png",
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 30),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Home(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 40),
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Color(0xff3280ef),
                       borderRadius: BorderRadius.circular(30),
-                      child: Container(
-                        height: 60,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          color: Color(0xff3280ef),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      
-                        child: Center(
-                          child: Text(
-                            "Get Started",
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 20),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
